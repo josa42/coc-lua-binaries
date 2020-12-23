89f9f9cc19347be908315ce2bbe0f8d097d33974
Josa Gesell <josa@gesell.me>
2020-12-23 12:56:45 +0100

    Add body to release

---
+++ .github/scripts/get_release_note.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+r="$(cd lua-language-server && git describe --tags --abbrev=0)"
+[[ "$r" == "" ]] && r="$(cd lua-language-server && cat changelog.md | grep '^##' | head -n1 | sed 's/^## //')"
+f=0;
+while read -r line; do
+  if [[ $line =~ '## 1.8.0' ]]; then
+    f=1;
+  elif [[ $f -eq 1 ]] && [[ $line =~ '##' ]]; then
+    f=0;
+  elif [[ $f -eq 1 ]]; then
+    echo "$line"
+  fi
+
+done < lua-language-server/changelog.md
+
