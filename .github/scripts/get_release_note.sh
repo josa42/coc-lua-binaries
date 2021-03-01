42d969c6689130a1951a6db4fd66221958483faa
Josa Gesell <josa@gesell.me>
2021-03-01 22:19:11 +0100

    Fix build

--- .github/scripts/get_release_note.sh
+++ .github/scripts/get_release_note.sh
@@ -4,7 +4,7 @@ r="$(cd lua-language-server && git describe --tags --abbrev=0)"
 [[ "$r" == "" ]] && r="$(cd lua-language-server && cat changelog.md | grep '^##' | head -n1 | sed 's/^## //')"
 f=0;
 while read -r line; do
-  if [[ $line =~ '## 1.8.0' ]]; then
+  if [[ $line = '## '$r ]]; then
     f=1;
   elif [[ $f -eq 1 ]] && [[ $line =~ '##' ]]; then
     f=0;
